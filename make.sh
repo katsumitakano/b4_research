b44dccdd27f88f62ee879e440e95444a26678f09
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-07-25 17:04:01 +0900

    実行用ファイルにコサイン類似度の計算も含める

--- make.sh
+++ make.sh
@@ -1,3 +1,10 @@
 # 実行用ファイル
-python makeMaterialTermsFromMXML.py
-python makeCoordinateMatrix.py
+python makedocs.py $1 docs.txt
+python makematrix.py
+python cosine.py
+# 以下のプログラムが追加されていく？
+# python lsi.py
+# python isomap.py
+# python lle.py
+# python deeplearning.py
+
