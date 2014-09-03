90ba064abf7db2419e94699679ec39fb5fbc99d7
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-09-03 17:10:58 +0900

    make.sh実行後にファイル郡を移動する処理追加

--- make.sh
+++ make.sh
@@ -1,7 +1,20 @@
-# 実行用ファイル
+#!/bin/sh
+
+# ディレクトリ名に使用
+DIR_DATE=`date +"%m%d_%H%M%S"`
+
+# 行列の作成と類似度の計算
 python makedocs.py $1 docs.txt
 python makematrix.py
 python cosine.py
+
+# 作成したデータをarchiveに移す
+mkdir archive/${DIR_DATE}
+cp docs.txt archive/${DIR_DATE}
+cp matrix.mat archive/${DIR_DATE}
+cp cosine.txt archive/${DIR_DATE}
+
+
 # 以下のプログラムが追加されていく？
 # python lsi.py
 # python isomap.py
