32c92c89a8dedbba4ae0f9be1adc4eecc5a02617
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-10-17 17:29:07 +0900

    makematrix.pyを高速化

--- makematrix.py
+++ makematrix.py
@@ -11,12 +11,13 @@ from mylib import measure_time
 @measure_time
 def makeMatrix( load_name='docs.txt', save_name='matrix.mat'):
     """
-    docs.txt からCoordinate形式の行列を作成する
+    docs.txt からlil形式の疎行列を作成する
     """
     
     # df_dict: Document Frequency Dictionaly
     df_dict   = {}   # 文脈頻度の辞書
-    dimention = 0    # 次元数（文脈数）
+    N = 0    # 単語数
+    M = 0    # 文脈数
 
     # 各単語の文脈での出現頻度を数える
     for line in open( load_name ):
@@ -25,7 +26,7 @@ def makeMatrix( load_name='docs.txt', save_name='matrix.mat'):
             df_dict[term] = df_dict.get(term, 0) + 1
 
         # 文脈数を数え上げる
-        dimention += 1
+        M += 1
 
     # n個以上の文脈に出現した単語だけを保持する
     threshold = 5 
@@ -34,20 +35,24 @@ def makeMatrix( load_name='docs.txt', save_name='matrix.mat'):
     terms = [item[0] for item in df_dict.iteritems() \
                         if  item[1] > threshold]
 
+    # 文書をリストに格納しておく（docsが100MB程度なら問題ない）
+    documents = [ line.rstrip().split(',') \
+                      for line in open(load_name) ]
+
     # lil_matrix形式の疎行列を生成
-    len_terms = len(terms)
-    lil_matrix = sparse.lil_matrix( (len_terms, dimention) )
+    N = len(terms)
+    lil_matrix = sparse.lil_matrix( (N, M) )
     for i, term in enumerate( terms ):
-        for j, line in enumerate( open(load_name) ):
-            counts = line.rstrip().split(',').count( term )
+        for j, line in enumerate( documents ):
+            counts = line.count( term ) #TODO:countの高速化を考える（正規表現？）
             if counts:
                 # TFの計算:文書に含まれる単語の数
                 tf  = float(counts)
                 # IDFの計算:log(全文書数/その単語が含まれる文書数)
-                idf = math.log( float(dimention)/df_dict[term] )
+                idf = math.log( float(M)/df_dict[term] )
                 lil_matrix[i,j] = tf*idf
 
-        print "makematrix:%d/%d" % (i, len_terms) # 進捗確認
+        print "makematrix:%d/%d" % (i, N) # 進捗確認
     
     decoded_relation = map(lambda t: t.decode('utf_8'), terms) # decodeしないとloadmatができない
     io.savemat( save_name, {'matrix':lil_matrix, 'relation':decoded_relation} )
