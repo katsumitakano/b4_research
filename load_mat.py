5f8def09c393976440ecc920dcd2a6e1aefc34d5
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-10-18 12:11:30 +0900

    load_matを修正

--- load_mat.py
+++ load_mat.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 import numpy as np
 import scipy as sp
+from scipy import io
 
-mat_file = sp.io.loadmat('matrix.mat')
+mat_file = io.loadmat('matrix.mat')
 matrix   = mat_file['matrix']
 relation = mat_file['relation']
 
