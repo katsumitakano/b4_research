47715b4932bb1126f5ff9882970a1c738e4cf12e
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-10-28 21:04:46 +0900

    lleのプログラム完成

--- lle.py
+++ lle.py
@@ -2,66 +2,97 @@
 import numpy as np
 import scipy as sp
 import scipy.io as spio
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from scipy import sparse
+from scipy.sparse import linalg as splinalg
 from scipy.sparse import csgraph
 from sklearn import manifold
 from sklearn import datasets
 from sklearn.neighbors import NearestNeighbors
 
 def loadmat():
+    """
+    matrixファイルの読み込み
+    """
     matfile = spio.loadmat('matrix.mat')
     return (matfile['matrix'], \
             matfile['relation'])
 
-def knn_graph(spmat, k=4):
+def find_neighbours(spmat, k):
+    """
+    近傍点を探索し、その位置を返す
+    @p spmat: 疎行列形式の行列
+    @p k: 近傍点の個数
+    @r 各点に対応する近傍点の位置
+    """
     neigh = NearestNeighbors()
     neigh.fit(spmat)
-    dists, indices = neigh.kneighbors(spmat, k+1, return_distance=True)
-    dists   = dists[:,1:]   # 1列目は自分との距離なので使わない
-    indices = indices[:,1:] # 同上
+    indices = neigh.kneighbors(spmat, k+1, return_distance=False)
+    indices = indices[:,1:] # 1列目は自分との距離なので使わない
 
-    # 近傍探索グラフの作成
-    N = len(indices) # 距離行列の大きさ
-    G_sparse = sparse.lil_matrix( (N,N) )
-    for i, J in enumerate(indices):
-        for j, index in enumerate(J):
-            # TODO: 分かりづらいし遅い
-            G_sparse[i,index] = dists[i,j]
+    return indices
 
-    return G_sparse
+def solve_weights(spmat, neighs):
+    """
+    重み行列Wを返す
+    @p spmat: 疎行列形式の行列
+    @p neighs各点に対応する近傍点の位置
+    @r NxNの重み行列
+    """
+    k = neighs.shape[1] # 近傍点の個数
+    D = spmat.shape[1] # 元の行列の次元数
+    N = spmat.shape[0]
+    W = sparse.lil_matrix( (N,N) ) # 重み行列の宣言
+    ones = np.ones(k) # 方程式の解
 
-def MDS(D, n_components):
-    N = len(D)
-    S = D*D # 距離の2乗
-    H = np.eye(N) - np.ones((N,N))/N # 中心化行列
-    P = - 1.0/2 * H * S * H # ヤング・ハウスホルダー変換
+    for i, neigh in enumerate(neighs):
+        Z  = spmat[neigh].toarray()
+        Xi = spmat[i].toarray()
+        Z = Z-Xi
+        C = np.dot(Z, Z.T) # 分散共分散行列の計算
+        # import pdb;pdb.set_trace()
+        if k>D:
+            # 正規化するためCに正規化定数を足す
+            I = np.eye( C.shape[0] )
+            eps = 0.001*C.trace()
+            C += I*eps
+        w = sp.linalg.solve(C, ones, sym_pos=True)
+        w = w/sum(w) # 割って正規化
+        W[i, neigh] = w # 近傍点の解を格納
 
-    # スペクトル分解
-    eig_value, eig_vector = np.linalg.eig(P)
-    ind = np.argsort(eig_value)
-    p = [value for i, value \
-                   in enumerate(reversed(ind)) \
-                   if i < n_components]
+    return W.tocsr()
 
-    W = P.std(axis=0)[p] # 上位p個の固有値の標準偏差
-    X = eig_vector[:,p]  # 上位p個の固有ベクトル
-    
-    return W*X
+def embedding(spmat, W, d):
+    """
+    元の行列を低次元に埋め込む
+    @p spmat: 疎行列形式の行列
+    @p W: 重み行列
+    @p d: 圧縮後の次元数
+    @r 埋め込み後の行列
+    """
+    N = W.shape[0]
+    I = sparse.eye(N)
+    M = sp.dot( (I-W).T, (I-W) )
+
+    # sigmaがめちゃ大事らしい
+    eig_value, eig_vector = splinalg.eigsh(M, d+1,\
+                                           sigma=0.0,\
+                                           maxiter=100)
+    return eig_vector[:,1:]  # 底を除くd個の固有ベクトル
+
 
 if __name__ == "__main__":
-    k = 12   # 近傍点の数
-    n = 2   # 圧縮後の次元数
+    k = 12   # 近傍点の数（8~20程度）
+    d = 100   # 圧縮後の次元数
 
-    # スイスロールのデータ読み込み
-    swiss = datasets.make_swiss_roll(1000)
-    data  = swiss[0]
-    color = swiss[1]
+    # 疎行列データ読み込み
+    spmat, terms = loadmat()
 
-    # scikit-learnでテスト
-    lle = manifold.LocallyLinearEmbedding(n_neighbors=k,
-                             n_components=2)
-    _Y = lle.fit_transform(data)
-    plt.scatter(_Y[:,0], _Y[:,1], c=color)
-    plt.show()
+    # 1.近傍点の取得
+    neighs = find_neighbours(spmat, k)
+    
+    # 2.重み行列の計算
+    W = solve_weights(spmat, neighs)
+
+    # 3.低次元へ埋め込み
+    Y = embedding(spmat, W, d)
+
--- lle_test.py
+++ lle_test.py
@@ -12,73 +12,101 @@ from sklearn import datasets
 from sklearn.neighbors import NearestNeighbors
 
 def loadmat():
+    """
+    matrixファイルの読み込み
+    """
     matfile = spio.loadmat('matrix.mat')
     return (matfile['matrix'], \
             matfile['relation'])
 
-def knn_graph(spmat, k=4):
+def find_neighbours(spmat, k):
+    """
+    近傍点を探索し、その位置を返す
+    @p spmat: 疎行列形式の行列
+    @p k: 近傍点の個数
+    @r 各点に対応する近傍点の位置
+    """
     neigh = NearestNeighbors()
     neigh.fit(spmat)
-    dists, indices = neigh.kneighbors(spmat, k+1, return_distance=True)
-    dists   = dists[:,1:]   # 1列目は自分との距離なので使わない
-    indices = indices[:,1:] # 同上
+    indices = neigh.kneighbors(spmat, k+1, return_distance=False)
+    indices = indices[:,1:] # 1列目は自分との距離なので使わない
 
-    # 近傍探索グラフの作成
-    N = len(indices) # 距離行列の大きさ
-    G_sparse = sparse.lil_matrix( (N,N) )
-    for i, J in enumerate(indices):
-        for j, index in enumerate(J):
-            # TODO: 分かりづらいし遅い
-            G_sparse[i,index] = dists[i,j]
+    return indices
 
-    return G_sparse
+def solve_weights(spmat, neighs):
+    """
+    重み行列Wを返す
+    @p spmat: 疎行列形式の行列
+    @p neighs各点に対応する近傍点の位置
+    @r NxNの重み行列
+    """
+    k = neighs.shape[1] # 近傍点の個数
+    D = spmat.shape[1] # 元の行列の次元数
+    N = spmat.shape[0]
+    W = sparse.lil_matrix( (N,N) ) # 重み行列の宣言
+    ones = np.ones(k) # 方程式の解
+
+    for i, neigh in enumerate(neighs):
+        Z  = spmat[neigh].toarray()
+        Xi = spmat[i].toarray()
+        Z = Z-Xi
+        C = np.dot(Z, Z.T) # 分散共分散行列の計算
+        # import pdb;pdb.set_trace()
+        if k>D:
+            # 正規化するためCに正規化定数を足す
+            I = np.eye( C.shape[0] )
+            eps = 0.001*C.trace()
+            C += I*eps
+        w = sp.linalg.solve(C, ones, sym_pos=True)
+        w = w/sum(w) # 割って正規化
+        W[i, neigh] = w # 近傍点の解を格納
+
+    return W.tocsr()
+
+def embedding(spmat, W, d):
+    """
+    元の行列を低次元に埋め込む
+    @p spmat: 疎行列形式の行列
+    @p W: 重み行列
+    @p d: 圧縮後の次元数
+    @r 埋め込み後の行列
+    """
+    N = W.shape[0]
+    I = sparse.eye(N)
+    M = sp.dot( (I-W).T, (I-W) )
+
+    # sigmaがめちゃ大事らしい
+    eig_value, eig_vector = splinalg.eigsh(M, d+1,\
+                                           sigma=0.0,\
+                                           maxiter=100)
+    return eig_vector[:,1:]  # 底を除くd個の固有ベクトル
 
 
 if __name__ == "__main__":
-    k = 20   # 近傍点の数
-    n = 2   # 圧縮後の次元数
+    k = 12   # 近傍点の数（8~20程度）
+    d = 2   # 圧縮後の次元数
 
     # スイスロールのデータ読み込み
-    swiss = datasets.make_swiss_roll(500)
+    swiss = datasets.make_swiss_roll(1000)
     data  = swiss[0]
     color = swiss[1]
 
-    # 近傍グラフの作成
+    # 近傍点の取得
     spmat = sparse.csr_matrix(data)
-    Z = knn_graph(spmat, k)
-
-    # 共分散行列の計算
-    C = Z.T*Z
-
-    # 重み行列Wの計算
-    N = C.shape[0]
-    w = splinalg.spsolve(C, np.ones(N))
-    wsum = np.sum(w)
-    W = sparse.lil_matrix( (N,N) )
-    for i, J in enumerate(Z.rows):
-        for j in J:
-            W[i,j] = w[i]/wsum # 遅いはず
+    neighs = find_neighbours(spmat, k)
     
-    W = W.tocsr()
-    I = sparse.csr_matrix( np.eye(N) )
-    IW = (I-W)
-    M = IW.T * IW
+    # 重み行列の計算
+    W = solve_weights(spmat, neighs)
 
-    # スペクトル分解
-    eig_value, eig_vector = np.linalg.eig(M.toarray())
-    ind = np.argsort(eig_value)
-    p = ind[1:n+1]
+    # 低次元へ埋め込み
+    Y = embedding(spmat, W, d)
 
-    Va = eig_value[p]
-    Ve = eig_vector[:,p]  # 上位p個の固有ベクトル
-    
-    Y = Va*Ve
     plt.scatter(Y[:,0], Y[:,1], c=color)
     plt.show()
 
     # scikit-learnでテスト
     # lle = manifold.LocallyLinearEmbedding(n_neighbors=k,
-    #                          n_components=2)
+    #                          n_components=d)
     # _Y = lle.fit_transform(data)
     # plt.scatter(_Y[:,0], _Y[:,1], c=color)
     # plt.show()
