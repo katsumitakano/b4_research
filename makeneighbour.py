bd51148e1948dad42b5b819abc4be1fa0ddc8905
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-10-30 15:25:44 +0900

    makeneighbour.pyを高速化

--- makeneighbour.py
+++ makeneighbour.py
@@ -1,33 +1,35 @@
-# coding: utf-8
+ # coding: utf-8
 import os
 import sys
 import numpy as np
 import scipy as sp
 from scipy import io as spio
+from sklearn.neighbors import NearestNeighbors
 
 if __name__ == "__main__":
     mat_file = spio.loadmat('matrix.mat')
     matrix   = mat_file['matrix']
-    relation = mat_file['relation']
+    # relation = mat_file['relation']
 
     save_name = "neighbours.dat"
     if  os.path.isfile( save_name ):
         os.remove( save_name )
     wfile = open ( save_name, 'a' )
 
-    N = matrix.shape[0]
-    dists = np.empty(N, dtype=np.float64)
+    k = 100 # 近傍点の個数
+    N = matrix.shape[0] # データ数
+
+    # フィッティング
+    neigh = NearestNeighbors()
+    neigh.fit(matrix)
     for i in xrange(N):
-        A = matrix[i]
-        for j in xrange(N):
-            # 距離計算
-            B = matrix[j]
-            d = np.linalg.norm( (A-B).toarray() )
-            dists[j] = d
-        # 書き込み
-        ind = dists.argsort()[:100]
+        # 近傍点取得
+        dists, indices = neigh.kneighbors(matrix[i], k,\
+                                          return_distance=True)
+        # 書き込み用に整形
         write_list = []
-        for index, dist in zip(ind, dists[ind]):
-            write_list.append("%d:%.4f" % (index, dist))
+        for ind, dist in zip(indices.ravel(), dists.ravel()):
+            write_list.append("%d:%.4f" % (ind, dist))
+        # 書き込み
         wfile.write( ' '.join(write_list)+'\n' )
         sys.stderr.write("makeneighbour:%d\n" % (i)) # 進捗確認
