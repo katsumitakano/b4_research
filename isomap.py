a65a035f995387d815e762074fa0855402889393
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-12-04 16:23:47 +0900

    Isomapで近傍半径に対応
    isomap.py, lle.pyともに固有値計算前の行列保存処理をコメントアウト
    必要なときに適宜使うようにする

--- isomap.py
+++ isomap.py
@@ -18,6 +18,44 @@ def loadmat():
             matfile['terms'])
 
 @measure_time
+def rnn_graph(spmat, r, test=False):
+    """
+    疎な近傍グラフを作成する
+    @p spmat: 疎行列形式の行列
+    @p r: 近傍半径
+    @r 疎行列形式の近傍グラフ
+    """
+    N = spmat.shape[0] # 距離行列の大きさ
+    G_sparse = sparse.lil_matrix( (N,N) )
+
+    if test:
+        # --- テスト時はneighborsをそのまま使う
+        neigh = NearestNeighbors()
+        neigh.fit(spmat)
+        dists, indices = neigh.radius_neighbors(spmat, r, return_distance=True)
+
+        # 近傍探索グラフの作成
+        for i, J in enumerate(indices):
+            G_sparse[i, J[1:]] = dists[i][1:] # FancyIndex(自分との距離は使わない)
+    else:
+        # --- 本番時はファイルから読み込む
+        neighs = np.loadtxt('neighbours.dat', delimiter=" ", dtype="S")
+
+        # 近傍探索グラフの作成
+        for i, neigh in enumerate(neighs):
+            for idx_dist in neigh[1:]:
+                idx, dist = idx_dist.split(":")
+                idx  = int(idx)
+                dist = float(dist)
+                if dist > r:
+                    break # 距離が近傍半径を超えたら終了
+                else:
+                    G_sparse[i, idx] = dist
+            sys.stderr.write("rnn_graph:%d\n" % (i))
+
+    return G_sparse
+
+@measure_time
 def knn_graph(spmat, k, test=False):
     """
     疎な近傍グラフを作成する
@@ -66,7 +104,7 @@ def MDS(D, d):
     S = D*D # 距離の2乗
     H = np.eye(N) - np.ones((N,N))/N # 中心化行列
     P = -0.5 * H.dot(S).dot(H) # ヤング・ハウスホルダー変換(-1/2*H*S*H)
-    np.save("P_isomap", P) # 行列保存
+    #np.save("P_isomap", P) # 行列保存
 
     # 固有値計算
     sys.stderr.write("Start Eigen Computation\n")
@@ -81,7 +119,7 @@ def MDS(D, d):
     
     return np.sqrt(W)*X
 
-def Isomap(spmat, k, d, test=False):
+def Isomap(spmat, k=None, r=None, d=300, test=False):
     """
     Isomapの実行
     @p spmat: 疎行列形式の行列
@@ -92,8 +130,17 @@ def Isomap(spmat, k, d, test=False):
     INFVAL = 10000 # 適当に大きい値(np.infと交換)
 
     # 近傍点の探索
-    sys.stderr.write("making knn_graph\n")
-    G_sparse = knn_graph(spmat, k, test)
+    if   k != None: # k近傍の実行
+        sys.stderr.write("making knn_graph\n")
+        G_sparse = knn_graph(spmat, k, test)
+
+    elif r != None: # 近傍半径の実行
+        sys.stderr.write("making rnn_graph\n")
+        G_sparse = rnn_graph(spmat, r, test)
+
+    else: # 正しい引数が得られていない
+        sys.stderr.write("Input k or r Value\n")
+        sys.exit()
 
     # 測地線距離に基づく距離行列D_Gを作成
     sys.stderr.write("compute distance matrix\n")
@@ -104,15 +151,10 @@ def Isomap(spmat, k, d, test=False):
     sys.stderr.write("embedding with MDS...\n")
     Y = MDS(D_G, d)
 
-    # scikit-learnの多次元尺度構成法
-    # n_jobs: 並列化の数（-1で全てのCPU）
-    #mds = manifold.MDS(n_components=d, n_jobs=-1, dissimilarity="precomputed")
-    #Y = mds.fit_transform(D_G)
-
     sys.stderr.write("OK!\n")
     return Y
 
-def test(k, d):
+def test(k, r, d=2):
     """
     テスト実行
     """
@@ -124,7 +166,7 @@ def test(k, d):
     color = swiss[1]
     spmat = sparse.csr_matrix(data)
     # Isomap実行
-    Y = Isomap(spmat, k, d, test=True)
+    Y = Isomap(spmat, k, r, d, test=True)
     # 2次元プロット
     plt.scatter(Y[:,0], Y[:,1], c=color)
     plt.show()
@@ -133,20 +175,42 @@ def test(k, d):
 if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv)
+    usage = "Usage: isomap.py [-k|-r] 近傍点or近傍半径 圧縮後の次元 [test]\n"
 
-    if argc != 3 and argc != 4:
-        sys.stderr.write("Usage: isomap.py 近傍数 圧縮後の次元 [test]\n")
+    if argc != 4 and argc != 5:
+        sys.stderr.write(usage)
         sys.exit()
 
-    k = int(argv[1]) # 近傍点の数
-    d = int(argv[2]) # 圧縮後の次元数
+    t = argv[1]      # 近傍のタイプ
+    v = int(argv[2]) # その値
+    d = int(argv[3]) # 圧縮後の次元数
 
-    if argc == 3:   # 通常時
+    # -------------------------
+    # 通常時の処理
+    # -------------------------
+    if argc == 4:
         spmat, terms = loadmat()
-        Y = Isomap(spmat, k, d, test=False)
-        save_name = "isomap_k%d_d%d.mat" % (k, d)
-        spio.savemat(save_name, {'Y':Y, 'terms':terms})
-    elif argc == 4: # テスト実行
-        test(k, d)
+        if t == "-k":
+            Y = Isomap(spmat, k=v, d=d, test=False) # k近傍
+            save_name = "isomap_k%d_d%d.mat" % (v, d)
+        elif t == "-r":
+            Y = Isomap(spmat, r=v, d=d, test=False) # 近傍半径
+            save_name = "isomap_r%f_d%d.mat" % (v, d)
+        else:
+            sys.stderr.write(usage)
+            sys.exit()
 
+        spio.savemat(save_name, {'Y':Y, 'terms':terms})
+
+    # -------------------------
+    # テスト時の処理
+    # -------------------------
+    elif argc == 5:
+        if t == "-k":
+            test(k=v, r=None, d=d)
+        elif t == "-r":
+            test(k=None, r=v, d=d)
+        else:
+            sys.stderr.write(usage)
+            sys.exit()
         
