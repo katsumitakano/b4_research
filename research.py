dcb919f81a8c0762452c509d6270f0365d653600
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-12-05 16:19:25 +0900

    research.py:距離による評価でも類似度を出せる

--- research.py
+++ research.py
@@ -6,10 +6,11 @@ from scipy import io
 from numpy.linalg import norm
 
 # ---------- ファイルの読み込み ---------- #
-filename = raw_input("Input matrix name (***.mat) -> ")
+filename = sys.argv[1]
 
 try:
     mat_file = io.loadmat(filename)
+    print("Matfile loaded!")
 except:
     sys.stderr.write("Not correct matrix name!\n")
     sys.exit()
@@ -24,12 +25,16 @@ def sim_cosine(vector1, vector2):
     """コサイン類似度を返す"""
     return np.dot(vector1, vector2)/(norm(vector1)*norm(vector2))
 
-def print_top(term, n):
+def sim_euclid(vector1, vector2):
+    """ユークリッド距離を返す（値が小さいほうが近い）"""
+    return -norm(vector1 - vector2)
+
+def print_top(term, n, sim_func=sim_cosine):
     """与えられた単語と類似度が高い上位n件を表示"""
     index = terms.index(term)
     tgt_vec = matrix[index]
     # 全単語との類似度を計算
-    sims = np.array([sim_cosine(tgt_vec, vec) for vec in matrix])
+    sims = np.array([sim_func(tgt_vec, vec) for vec in matrix])
     # 類似度の高い順にソート
     ranking = sims.argsort()[::-1]
     # 上位n件の位置と類似度を取得（1つ目は同じ単語なので無視）
@@ -41,6 +46,7 @@ def print_top(term, n):
 
 
 # ---------- 自作インタプリターの起動 ---------- #
+sim_func = sim_cosine
 while(1):
     input_line = raw_input("terms? -> ").decode('utf-8')
     input_list = input_line.split()
@@ -54,6 +60,16 @@ while(1):
         print "Good Bye"
         sys.exit()
 
+    elif input_list[0] in [ ":c", ":cosine" ]:
+        # 類似度計算をコサイン類似度に変更
+        print("Change simType to \"cosine\"")
+        sim_func = sim_cosine; continue
+
+    elif input_list[0] in [ ":e", ":euclid" ]:
+        # 類似度計算をユークリッド距離に変更
+        print("Change simType to \"euclid\"")
+        sim_func = sim_euclid; continue
+
     elif len(input_list) == 1:
         # 単語1つ： 類似度が高い上位n件を表示
         try:
@@ -61,7 +77,7 @@ while(1):
         except ValueError:
             print("No such term: %s" % (input_list[0]))
             continue
-        print_top(input_list[0], 10)
+        print_top(input_list[0], 10, sim_func)
 
     elif len(input_list) == 2:
         # 単語2つ： 2単語間のコサイン類似度を計算
@@ -77,4 +93,5 @@ while(1):
         print sim_cosine(matrix[ind1], matrix[ind2])
 
     else:
+        # 引数エラー
         print("調べたい単語を1つ、または2つ入力して下さい。")
