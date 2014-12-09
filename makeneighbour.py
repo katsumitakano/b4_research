d38ae5763e51735aa4d12dc1e5259c9f55c947b7
katsumi <katsumi@utm.inf.uec.ac.jp>
2014-12-09 19:56:33 +0900

    全ての単語との近傍点をファイルに書き出す
    r近傍に対応するため
    問題点：データサイズが大きくなる

--- makeneighbour.py
+++ makeneighbour.py
@@ -10,27 +10,25 @@ from mylib import measure_time
 if __name__ == "__main__":
     mat_file = spio.loadmat('matrix.mat')
     matrix   = mat_file['matrix']
-    # relation = mat_file['relation']
 
     save_name = "neighbours.dat"
     if  os.path.isfile( save_name ):
         os.remove( save_name )
     wfile = open ( save_name, 'a' )
 
-    k = 500 # 近傍点の個数
     N = matrix.shape[0] # データ数
 
     # フィッティング
     neigh = NearestNeighbors()
     neigh.fit(matrix)
     for i in xrange(N):
-        # 近傍点取得
-        dists, indices = neigh.kneighbors(matrix[i], k,\
+        # 近傍点取得(全単語との距離を格納)
+        dists, indices = neigh.kneighbors(matrix[i], N,\
                                           return_distance=True)
         # 書き込み用に整形
         write_list = []
         for ind, dist in zip(indices.ravel(), dists.ravel()):
-            write_list.append("%d:%.20g" % (ind, dist)) # 精度は適当…
+            write_list.append("%d:%.10g" % (ind, dist)) # 精度は適当…
         # 書き込み
         wfile.write( ' '.join(write_list)+'\n' )
         sys.stderr.write("makeneighbour:%d\n" % (i)) # 進捗確認
