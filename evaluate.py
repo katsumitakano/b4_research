60fcadc8a751e4e724e220b1ff167f68aba81a7e
katsumi <katsumi@utm.inf.uec.ac.jp>
2015-01-12 13:37:10 +0900

    評価の種類をコマンドラインから選択できる

--- evaluate.py
+++ evaluate.py
@@ -14,9 +14,9 @@ def sim_cosine(vector1, vector2):
     return np.dot(vector1, vector2)/(norm(vector1)*norm(vector2))
 
 
-def F_ave( matrix, relation, n_max ):
+def Retrieval_eval( matrix, relation, n_max, eval_type ):
     """
-    n_maxまでの平均F値の計算
+    n_maxまでの平均F値、または平均再現率の計算
     """
     M = matrix['Y']
     terms = matrix['terms']
@@ -54,21 +54,22 @@ def F_ave( matrix, relation, n_max ):
             Recall    = float(R)/C # 再現率
             Precision = float(R)/N # 適合率
             F = 2.0*R/(C+N)        # F値（式変形）
-            tmp.append(F)
+            if   eval_type == "F": tmp.append(F)
+            elif eval_type == "R": tmp.append(Recall)
 
         result.append( np.array(tmp).mean() )
 
     return np.array(result).mean()
 
-def rank_mean( matrix, relation ):
+def Rank_eval( matrix, relation, eval_type ):
     """
-    分類語のランクの中央値を計算
+    分類語のランクの平均値、または中央値を計算
     """
     M = matrix['Y']
     terms = matrix['terms']
     terms = map(lambda t: t.rstrip(), terms) # 末尾の空白削除
 
-    result = [] # F_nの値を保持
+    result = []
     sigeki_bunrui = relation.dropna()
 
     for sigeki in sigeki_bunrui.index:
@@ -96,11 +97,13 @@ def rank_mean( matrix, relation ):
             result.append( rank )
             #print bun, rank
 
-    #return np.array(result).mean()
-    return int(np.median(np.array(result)))
+    if eval_type == "mean":
+        return np.array(result).mean()
+    elif eval_type == "median":
+        return int(np.median(np.array(result)))
 
 @measure_time
-def evaluate( mat_name ):
+def evaluate( mat_name, eval_type ):
     """
     全関係の評価値を計算する
     """
@@ -110,24 +113,35 @@ def evaluate( mat_name ):
 
     # 行列ファイルを読み込み、
     matrix = spio.loadmat( mat_name )
-    #df_matrix = DataFrame( matrix['Y'], index=matrix['terms'] )
 
     # 上位10%までみる
     n_max = 100 * ( len(matrix['terms']) / 1000 )
 
-    # 全関係で平均F値を取る
+    # 全関係で評価値を取る
     for column in relation.columns:
         print column
-        #print F_ave( matrix, relation[column], n_max )
-        print rank_mean( matrix, relation[column] )
+        if eval_type in ["F", "R"]:
+            # F値または再現率の評価
+            print Retrieval_eval( matrix, relation[column], n_max, eval_type )
+        elif eval_type in ["mean", "median"]:
+            # ランクの平均値または中央値の評価
+            print Rank_eval( matrix, relation[column], eval_type )
 
 
 if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv)
 
-    if argc != 2:
-        sys.stderr.write("Usage: python evaluate.py 行列ファイル名\n")
-        sys.exit
+    if argc != 3:
+        sys.stderr.write("Usage: python evaluate.py 行列ファイル名 -評価指標\n")
+        sys.exit()
     else:
-        evaluate( mat_name=argv[1] )
+        mat_name = argv[1]
+        eval_type = argv[2][1:]
+
+        # 評価のタイプをチェック
+        if eval_type not in ["F", "R", "mean", "median"]:
+            sys.stderr.write("eval_type: F or R or mean or median\n")
+            sys.exit()
+
+        evaluate( mat_name=argv[1], eval_type=eval_type )
